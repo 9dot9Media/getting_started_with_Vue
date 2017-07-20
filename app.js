0157315d2b1c612929e882b9293ef2cdbaee1252
Kshitij Sobti <kshitij@sobti.in>
2017-07-20 19:25:50 +0530

    Implemented Vue.js GitHub user search app as monoloth.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const app = new Vue({
     el: '#app',
-    template: '<h1>{{ greeting }} World!</h1>',
+    template: `<div>
+                <input v-model="query" @keyup.enter="search" />
+                <button @click="search">Search</button>
+                <ul>
+                <li v-for="user in users" :key="user.id">
+                    <img :src="user.avatar_url" height="128"/>
+                    <a :href="user.html_url">{{ user.login }}</a>
+                    <span v-if="user.type == 'User'">(U)</span>
+                    <span v-else>(O)</span>
+                </li>
+                </ul>
+            </div>`,
     data: {
-        greeting: 'Hello'
+        users: [],
+        query: ''
+    },
+    methods: {
+        search: function() {
+            fetch('https://api.github.com/search/users\?q\=' + this.query)
+                .then((response) => response.json())
+                .then((json) => { this.users = json.items });
+        }
     }
 });
